7de1e97c14cc7ebf697e5e8b1052c57ec24a2118
B1ueber2y <b1ueber2y@gmail.com>
2022-05-19 16:20:20 +0200

    minor.

--- limap/line2d/register_matcher.py
+++ limap/line2d/register_matcher.py
@@ -1,15 +1,4 @@
 
-def get_detector(cfg_detector, max_num_2d_segs=3000):
-    method = cfg_detector["method"]
-    if method == "lsd":
-        from .LSD import LSDDetector
-        return LSDDetector(set_gray=True, max_num_2d_segs=max_num_2d_segs)
-    elif method == "sold2":
-        from .SOLD2 import SOLD2Detector
-        return SOLD2Detector(set_gray=True, max_num_2d_segs=max_num_2d_segs)
-    else:
-        raise NotImplementedError
-
 def get_matcher(cfg_matcher, extractor, n_neighbors=20):
     method = cfg_matcher["method"]
     if method == "sold2":
