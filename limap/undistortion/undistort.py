15b4a9e3a252d220c2a14f018cae06ae9730e1ef
B1ueber2y <b1ueber2y@gmail.com>
2022-06-02 19:56:51 +0200

    minor.

--- limap/undistortion/undistort.py
+++ limap/undistortion/undistort.py
@@ -12,17 +12,3 @@ def UndistortImageCamera(camera, imname_in, imname_out):
     camera_undistorted = _undistortion._UndistortCamera(imname_in, camera, imname_out)
     return camera_undistorted
 
-def UndistortImageCamera_OPENCV(camera, imname_in, imname_out):
-    img = cv2.imread(imname_in)
-    imsize = (img.shape[1], img.shape[0])
-    K = camera.K
-    coeffs = np.array(camera.dist_coeffs)
-    map1, map2 = cv2.initUndistortRectifyMap(K, coeffs, np.eye(3), K, imsize, cv2.CV_16SC2)
-    img_undistorted = cv2.remap(img, map1, map2, interpolation=cv2.INTER_NEAREST, borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0, 0))
-    cv2.imwrite(imname_out, img_undistorted)
-    # deepcopy
-    camera_undistorted = _base.Camera(camera.K, camera.R, camera.T)
-    camera_undistorted.set_hw(camera.h, camera.w)
-    return camera_undistorted
-
-
