14a994a85cb7160124e8aaa0614823a44a50b471
B1ueber2y <b1ueber2y@gmail.com>
2022-07-04 00:14:42 +0200

    bugfix for subset sampling on ETH3D.

--- limap/pointsfm/functions.py
+++ limap/pointsfm/functions.py
@@ -5,29 +5,13 @@ def filter_by_cam_id(cam_id, prev_imagecols, prev_neighbors):
     Filter _base.ImageCollection by cam id
     '''
     assert prev_imagecols.NumImages() == len(prev_neighbors)
-    neighbors = []
-    prev_cameras, prev_camimages = prev_imagecols.get_cameras(), prev_imagecols.get_images()
-    camimages = []
-    # filter ids
-    id_maps = {}
-    for idx in range(prev_imagecols.NumImages()):
-        camimage = prev_imagecols.camimage(idx)
-        cam_id_idx = camimage.cam_id
-        if cam_id_idx != cam_id:
-            id_maps[idx] = -1
+    valid_image_ids = []
+    for img_id in prev_imagecols.get_img_ids():
+        if prev_imagecols.camimage(img_id).cam_id != cam_id:
             continue
-        id_maps[idx] = len(camimages)
-        camimages.append(camimage)
-        neighbors.append(prev_neighbors[idx])
-    # map ids for neighbors
-    cameras = {}
-    cameras[cam_id] = prev_imagecols.cam(cam_id);
-    imagecols = _base.ImageCollection(cameras, camimages)
-    for idx in range(len(camimages)):
-        n0 = neighbors[idx]
-        n1 = [id_maps[k] for k in n0]
-        n2 = [k for k in n1 if k != -1]
-        neighbors[idx] = n2
+        valid_image_ids.append(img_id)
+    imagecols = prev_imagecols.subset_by_image_ids(valid_image_ids)
+    neighbors = imagecols.update_neighbors(prev_neighbors)
     return imagecols, neighbors
 
 def ComputeNeighborsVector(model, n_neighbors, min_triangulation_angle=1.0, neighbor_type="iou"):
