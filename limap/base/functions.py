0dfc6898a9f7ca52575e78045083008e077c208f
B1ueber2y <b1ueber2y@gmail.com>
2022-08-20 12:20:14 +0200

    minor.

--- limap/base/functions.py
+++ limap/base/functions.py
@@ -12,7 +12,7 @@ def get_all_lines_3d(seg3d_list):
         all_lines_3d[img_id] = _base._GetLine3dVectorFromArray(segs3d)
     return all_lines_3d
 
-def get_lines_to_track_map(all_lines_2d, linetracks):
+def get_invert_idmap_from_linetracks(all_lines_2d, linetracks):
     map = {}
     for img_id in all_lines_2d:
         lines_2d = all_lines_2d[img_id]
@@ -21,4 +21,4 @@ def get_lines_to_track_map(all_lines_2d, linetracks):
         for img_id, line_id in zip(track.image_id_list, track.line_id_list):
             map[img_id][line_id] = track_id
     return map
-    
+
