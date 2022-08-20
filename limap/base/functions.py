f2880d08e51ccf48e151346b57a997169603f449
Yifan Yu <markyu98@outlook.com>
2022-08-20 17:34:21 +0800

    Add function to map 2D line to linetrack

--- limap/base/functions.py
+++ limap/base/functions.py
@@ -12,3 +12,12 @@ def get_all_lines_3d(seg3d_list):
         all_lines_3d[img_id] = _base._GetLine3dVectorFromArray(segs3d)
     return all_lines_3d
 
+def get_lines_to_track_map(all_lines_2d, linetracks):
+    map = {}
+    for img_id in all_lines_2d:
+        lines_2d = all_lines_2d[img_id]
+        map[img_id] = [-1] * len(lines_2d)
+    for track_id, track in enumerate(linetracks):
+        for img_id, line_id in zip(track.image_id_list, track.line_id_list):
+            map[img_id][line_id] = track_id
+    return map
