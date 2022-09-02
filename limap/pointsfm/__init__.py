21d4fbd37ed0fe52d48a088a66573a607f488355
B1ueber2y <b1ueber2y@gmail.com>
2022-09-02 22:18:38 +0200

    minor. remove pybind11::embed from Cmakelists.

--- limap/pointsfm/__init__.py
+++ limap/pointsfm/__init__.py
@@ -1,4 +1,5 @@
 from _limap._pointsfm import *
 from .colmap_sfm import *
 from .functions import *
+from .colmap_reader import PyReadCOLMAP
 
--- limap/pointsfm/colmap_reader.py
+++ limap/pointsfm/colmap_reader.py
@@ -39,3 +39,26 @@ def ReadInfos(model, colmap_path, model_path="sparse", image_path="images"):
     imagecols = _base.ImageCollection(cameras, camimages)
     return imagecols
 
+def PyReadCOLMAP(colmap_path, model_path=None):
+    if model_path is not None:
+        model_path = os.path.join(colmap_path, model_path)
+    else:
+        model_path = colmap_path
+    if os.path.exists(os.path.join(model_path, "points3D.bin")):
+        fname_cameras = os.path.join(model_path, "cameras.bin")
+        fname_points = os.path.join(model_path, "points3D.bin")
+        fname_images = os.path.join(model_path, "images.bin")
+        colmap_cameras = read_cameras_binary(fname_cameras)
+        colmap_images = read_images_binary(fname_images)
+        colmap_points = read_points3D_binary(fname_points)
+    elif os.path.exists(os.path.join(model_path, "points3D.txt")):
+        fname_cameras = os.path.join(model_path, "cameras.txt")
+        fname_points = os.path.join(model_path, "points3D.txt")
+        fname_images = os.path.join(model_path, "images.txt")
+        colmap_cameras = read_cameras_text(fname_cameras)
+        colmap_images = read_images_text(fname_images)
+        colmap_points = read_points3D_text(fname_points)
+    else:
+        raise ValueError("Error! The model file does not exist at {0}".format(model_path))
+    return colmap_cameras, colmap_images, colmap_points
+
