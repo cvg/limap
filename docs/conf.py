37dabb895614e0b10b30d6abd481f24d0600b41a
Yifan Yu <markyu98@outlook.com>
2023-04-04 02:47:18 +0200

    Support google style docstring

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,8 @@ release = version
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
