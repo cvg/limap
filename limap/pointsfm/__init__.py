d62554da484022a345cc6050e8dd366e1891170a
B1ueber2y <b1ueber2y@gmail.com>
2024-11-20 17:00:31 +0100

    fix init

--- limap/pointsfm/__init__.py
+++ limap/pointsfm/__init__.py
@@ -1,3 +1,4 @@
+from _limap import _pointsfm as _pointsfm
 from _limap._pointsfm import *  # noqa: F403
 
 from .colmap_reader import (
@@ -17,3 +18,17 @@ from .functions import (
     read_infos_colmap,
     read_infos_visualsfm,
 )
+
+__all__ = [n for n in _pointsfm.__dict__ if not n.startswith("_")] + [
+    "PyReadCOLMAP",
+    "ReadPointTracks",
+    "check_exists_colmap_model",
+    "run_colmap_sfm",
+    "run_colmap_sfm_with_known_poses",
+    "run_hloc_matches",
+    "compute_metainfos",
+    "compute_neighbors",
+    "read_infos_bundler",
+    "read_infos_colmap",
+    "read_infos_visualsfm",
+]
