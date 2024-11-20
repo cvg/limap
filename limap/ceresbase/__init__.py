2f5c5e5f2b15d3ee119a95497d7c01707327736d
B1ueber2y <b1ueber2y@gmail.com>
2024-11-20 17:28:23 +0100

    finish ceresbase

--- limap/ceresbase/__init__.py
+++ limap/ceresbase/__init__.py
@@ -1,1 +1,4 @@
-from _limap._ceresbase import *
+from _limap import _ceresbase as _ceresbase
+from _limap._ceresbase import *  # noqa: F403
+
+__all__ = [n for n in _ceresbase.__dict__ if not n.startswith("_")]
