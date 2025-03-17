390564281f5d518b389f2f29e3124bf95149621e
B1ueber2y <b1ueber2y@gmail.com>
2025-03-17 16:51:30 +0100

    try

--- cmake/FindDependencies.cmake
+++ cmake/FindDependencies.cmake
@@ -54,6 +54,7 @@ endif()
 message(STATUS "Configuring PoseLib... done")
 
 # COLMAP
+set(GUI_ENABLED OFF)
 FetchContent_Declare(COLMAP
     GIT_REPOSITORY    https://github.com/colmap/colmap.git
     GIT_TAG           c9097b031ee00da22609c7ac4b3f6b45b4178de2
