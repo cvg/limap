2e2adc97adbdc958b3dfd00f1261f738d1268cda
Shaohui Liu <b1ueber2y@gmail.com>
2025-07-15 10:57:26 +0200

    format

--- tests/third-party/test_pycolmap.py
+++ tests/third-party/test_pycolmap.py
@@ -5,7 +5,10 @@ import pytest
 @pytest.mark.ci_workflow
 def test_pycolmap():
     syn_options = pycolmap.SyntheticDatasetOptions(
-        num_rigs=1, num_cameras_per_rig=3, num_frames_per_rig=8, num_points3D=100
+        num_rigs=1,
+        num_cameras_per_rig=3,
+        num_frames_per_rig=8,
+        num_points3D=100,
     )
     recon = pycolmap.synthesize_dataset(syn_options)
     assert recon.num_cameras() == 3
