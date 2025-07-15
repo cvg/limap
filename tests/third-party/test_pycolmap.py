c17ebf25b1d43dbbcbb3e8f208e20468853027d4
Shaohui Liu <b1ueber2y@gmail.com>
2025-07-15 10:54:19 +0200

    adapt to pycolmap 3.12

--- tests/third-party/test_pycolmap.py
+++ tests/third-party/test_pycolmap.py
@@ -5,9 +5,9 @@ import pytest
 @pytest.mark.ci_workflow
 def test_pycolmap():
     syn_options = pycolmap.SyntheticDatasetOptions(
-        num_cameras=3, num_images=8, num_points3D=100
+        num_rigs=1, num_cameras_per_rig=3, num_frames_per_rig=8, num_points3D=100
     )
     recon = pycolmap.synthesize_dataset(syn_options)
     assert recon.num_cameras() == 3
-    assert recon.num_images() == 8
+    assert recon.num_images() == 24
     assert recon.num_points3D() == 100
