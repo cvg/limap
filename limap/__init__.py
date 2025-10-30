f75c8eaebb45ab0cddfcb0569d6c5822041fb2df
Shaohui Liu <b1ueber2y@gmail.com>
2025-10-30 09:48:07 +0100

    try

--- limap/__init__.py
+++ limap/__init__.py
@@ -1,6 +1,7 @@
 from . import _limap
-from ._limap import _base
 
-__all__ = []
+from . import base
+
+__all__ = ["_limap", "base"]
 __version__ = _limap.__version__
 __ceres_version__ = _limap.__ceres_version__
