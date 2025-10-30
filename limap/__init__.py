9da6a76e3e98f34a4d2ba904b0557a988b8ecd99
Shaohui Liu <b1ueber2y@gmail.com>
2025-10-30 23:40:35 +0100

    try.

--- limap/__init__.py
+++ limap/__init__.py
@@ -1,16 +1,7 @@
-from __future__ import annotations
+from . import _limap
+
 from . import base
 
-__all__ = ["__version__", "__ceres_version__", "_limap", "base"]
-
-import importlib
-
-
-def __getattr__(name: str):
-    if name == "_limap":
-        return importlib.import_module("._limap", __name__)
-    if name == "__version__":
-        return __getattr__("_limap").__version__
-    if name == "__ceres_version__":
-        return __getattr__("_limap").__ceres_version__
-    raise AttributeError(name)
+__all__ = ["__version__", "__ceres_version__", "base"]
+__version__ = _limap.__version__
+__ceres_version__ = _limap.__ceres_version__
