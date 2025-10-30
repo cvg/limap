e34f7f94807efcb3da6687b5fe4212ec6f8218b1
Shaohui Liu <b1ueber2y@gmail.com>
2025-10-30 21:34:11 +0100

    try

--- limap/__init__.py
+++ limap/__init__.py
@@ -1,5 +1,20 @@
-from . import _limap
+from __future__ import annotations
 
-__all__ = []
-__version__ = _limap.__version__
-__ceres_version__ = _limap.__ceres_version__
+__all__ = ["__version__", "__ceres_version__", "_limap"]
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
+
+import importlib
+
+
+def __getattr__(name: str):
+    if name == "_limap":
+        return importlib.import_module("._limap", __name__)
+    if name == "__version__":
+        return getattr(__getattr__("_limap"), "__version__")
+    if name == "__ceres_version__":
+        return getattr(__getattr__("_limap"), "__ceres_version__")
+    raise AttributeError(name)
